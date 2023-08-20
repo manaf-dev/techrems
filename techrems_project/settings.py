b6dbed4d803ed9fc7db6944cd9814fd680f124d1
rems <tech1.rems@gmail.com>
2023-08-20 08:16:00 +0000

    adding profile update forms

--- techrems_project/settings.py
+++ techrems_project/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL= '/media/'
+
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static'
 # ]
